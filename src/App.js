13dc909491cf382355b6bad85d163355c29d5ae8
kamalaak <kamalstiries007@gmail.com>
2022-07-21 14:23:35 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import {useState} from "react";
+import questions from "./componants/questions";
+import "./index.css"
+
+function App() {
+  const [questionind, setQuestion]=useState(0);
+  const curquestion = questions[questionind];
+  const [score, setscore]=useState(0);
+  const [screenscore, setscreenscore]=useState(false);
+  const selectoptions= (index)=>{
+
+    if (curquestion.snswer === index) {
+        setscore(score + 1);
+    }
+    const last = questionind+1;
+    if (last < questions.length) {
+      setQuestion(questionind +1);
+    }else{
+     setscreenscore(true)
+    }
+   
+  };
+  const some = () => {
+    setQuestion(0)
+    setscreenscore(false)
+    setscore(0)
+  };
+ 
+return(
+<div className="container">
+  {
+    screenscore ? (<><h1>Your score is :   {score}</h1>
+     <button className="btn" onClick={some}>do you wanna try again </button>
+    </>):( <div className="questions">
+    <p>{curquestion.questions} </p>
+   <div className="options">
+    <ul className="ul">{curquestion.options.map((Option, i) =>{
+      return <li className="li" onClick={()=>selectoptions(i)}>{Option} </li>
+    })}
+    </ul>
+   </div>
+  </div>)
+  };
+ </div>
+);
+}
+export default App;
---
+++ src/componants/questions.js
@@ -0,0 +1,21 @@
+export default[
+    {
+        questions: "what is html ",
+        options:["1", "2","3","4"],
+        snswer:0,
+    },
+    {
+        questions: "what is java ",
+        options:["1", "2","3","4"],
+        snswer:1,
+    }, {
+        questions: "what is python ",
+        options:["1", "2","3","4"],
+        snswer:2,
+    }, {
+        questions: "what is js ",
+        options:["1", "2","3","4"],
+        snswer:3,
+    },
+    
+]
